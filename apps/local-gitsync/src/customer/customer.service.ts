2c395f2a8b9473a09cb4609d504101d3536410db
Amit Barletz <amit@amplication.com>
2023-08-28 17:31:57 +0300

    custom code

--- apps/local-gitsync/src/customer/customer.service.ts
+++ apps/local-gitsync/src/customer/customer.service.ts
@@ -7,4 +7,18 @@ export class CustomerService extends CustomerServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
   }
+
+  async greetingCustomer(customerId: string) {
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    if (!customer) {
+      return "Customer not found";
+    }
+
+    return `Hello ${customer?.firstName} ${customer?.lastName}`;
+  }
 }
